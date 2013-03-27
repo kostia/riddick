e598a74f04893e61570a04bf31d9cf42d485eddf
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2013-03-28 00:14:32 +0100

    Removed code duplications from Backends facet

--- lib/riddick/backends.rb
+++ lib/riddick/backends.rb
@@ -7,15 +7,13 @@ module Riddick
       # Return a Riddick::Backends::Simple wrapped around a I18n::Backend::Simple.
       # It iterates over the backends in the chain and takes the first one found of required type.
       def simple
-        @simple ||= Riddick::Backends::Simple.new(
-            chain.backends.find { |be| be.kind_of? I18n::Backend::Simple })
+        @simple ||= Riddick::Backends::Simple.new(find_backend(I18n::Backend::Simple))
       end
 
       # Return a Riddick::Backends::Redis wrapped around a I18n::Backend::KeyValue.
       # It iterates over the backends in the chain and takes the first one found of required type.
       def key_value
-        @key_value ||= Riddick::Backends::Redis.new(
-            chain.backends.find { |be| be.kind_of? I18n::Backend::KeyValue })
+        @key_value ||= Riddick::Backends::Redis.new(find_backend(I18n::Backend::KeyValue))
       end
 
       # Returns the chain. By default it's I18n.backend (which is assumed to be a I18n::Backend::Chain).
@@ -32,6 +30,12 @@ module Riddick
       # Deletes a translation from the key-value backend.
       def delete_translation(key)
         key_value.delete_translation key
+      end
+
+      private
+
+      def find_backend(base_class)
+        chain.backends.find { |be| be.kind_of?(base_class) }
       end
     end
   end
